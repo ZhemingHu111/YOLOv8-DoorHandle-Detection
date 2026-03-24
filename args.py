d54bb41de616a98b12ae1c47355f67baa4a79a10
ZhemingHu111 <harryzheming@qq.com>
2026-03-25 05:13:33 +0800

    Add files via upload

--- args.py
+++ args.py
@@ -1,21 +1,17 @@
 import os
 
 def get_opts():
-
-    desktop_path = os.path.join(os.path.expanduser("~"), "Desktop", "door_handle")
+    image_dir = r"C:\Users\Boom\Desktop\door_handle\AI project image 2"
+    label_dir = r"C:\Users\Boom\Desktop\door_handle\obj_train_data"
     
     opts = {
-
-        "image_dir": os.path.join(desktop_path, "AI project image 2"),
-        
-
-        "label_dir": os.path.join(desktop_path, "obj_train_data"),
-        
-
-        "yaml_path": "./data.yaml",
-        
-        "model_type": "yolov8n.pt", 
+        "image_dir": image_dir,
+        "label_dir": label_dir,
+        "yaml_path": "data.yaml",
+        "model_type": "yolov8n.pt",
         "epochs": 50,
-        "imgsz": 640
+        "imgsz": 640,
+        "nc": 1,
+        "names": ['door_handle']
     }
     return opts
